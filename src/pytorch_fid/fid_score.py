0fed56a88872fb232b8661ef01021e1f420775ef
yadonglu <yadonglu@microsoft.com>
2023-02-21 19:58:02 +0000

    resize to 256

--- src/pytorch_fid/fid_score.py
+++ src/pytorch_fid/fid_score.py
@@ -118,7 +118,7 @@ def get_activations(files, model, batch_size=50, dims=2048, device='cpu',
                'Setting batch size to data size'))
         batch_size = len(files)
 
-    dataset = ImagePathDataset(files, transforms=TF.ToTensor())
+    dataset = ImagePathDataset(files, transforms=TF.Compose([TF.Resize(size=(256,256),interpolation=Image.BICUBIC), TF.toTensor()]))
     dataloader = torch.utils.data.DataLoader(dataset,
                                              batch_size=batch_size,
                                              shuffle=False,
